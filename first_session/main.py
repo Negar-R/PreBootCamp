460a90de105b9b4423a075c92e100404591593c2
Negar <negar.r.78.ioi@gmail.com>
2021-04-12 21:12:10 +0430

    this is our first session

---
+++ first_session/main.py
@@ -0,0 +1,61 @@
+x = 12
+y = "salam"
+
+# snake case
+my_age = 12
+
+# camel case
+myAge = 12
+
+# paskal case
+MyAge = 12
+
+my_name = "negar"
+my_char = "n"
+
+print("salam be hame")
+
+isIranian = False  # 0
+isIranian = True  # 1
+
+childs = None
+childs = "negar"
+
+print(type(childs))
+
+a = 23
+a = "hi"
+
+# int a = 1
+# a = "salam"
+
+x = 100
+y = 201
+
+# print("sum : ", x + y)
+# print("subtraction : ", x - y)
+# print("multiple : ", x * y)
+# print("divide : ", y / x)
+# print(x // y)
+# print("pow : ", x ** y)
+# print("remined : ", y % x)
+
+
+name = "negar"
+last_name = "rezaei"
+
+# print("salam " + name + " " + last_name)
+
+# format string
+name = "ali"
+last_name = "x"
+# format_string = f"salam {name}{last_name}"
+# print(format_string)
+
+name_1 = "zahra"
+f_string = "salam {0} family : {1}".format(name_1, last_name)
+print(f_string)
+
+
+""" doc string """
+x, y, z = 100, 200, 300
---
+++ first_session/note.py
@@ -0,0 +1,33 @@
+# print("salam man negaram")
+
+1. compiler & interpreter
+
+compiler : c++, c, java, c#
+interpreter: python, ruby, js, php
+
+differences :
+
+read source code
+memory
+speed
+debug hadling
+
+... -> bytcode -> machine
+... -> machine
+
+* pep -> 8, 427, 584 , ... 
+  pep 8 -> guide style
+
+  variable -> container -> bit
+
+Data type :
+1. integer 
+
+Dynamicaly typed language -> python : can change type and content of one variable 
+Static typed language -> c++ : can change content only
+
+
+data type :
+1. primitive -> int, str, bool
+2. refrence -> pointer -> object 
+
